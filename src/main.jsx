476ed9438c6cf7c7d6dba345d4b14dd24bbaf3b7
Mustafa9o <ali99329@gmail.com>
2026-01-11 11:24:50 +0300

    Add navigation between pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+﻿import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Main from './Main.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Main />
   </StrictMode>,
 )
